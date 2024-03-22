0406c8f2ffe231e113a8602c0b396797e5e5a5d0
faisal <ftokdiya@gmail.com>
2024-03-22 19:54:47 +0530

    initial commit

--- com.test/source_data_loading.py
+++ com.test/source_data_loading.py
@@ -10,14 +10,6 @@ if __name__ == '__main__':
         '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
     )
 
-    # Create the SparkSession
-    spark = SparkSession \
-        .builder \
-        .appName("Read com.test enterprise applications") \
-        .config("spark.mongodb.input.uri", "mongodb://ec2-18-202-237-95.eu-west-1.compute.amazonaws.com:27017") \
-        .getOrCreate()
-    spark.sparkContext.setLogLevel('ERROR')
-
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../" + "application.yml")
     app_secrets_path = os.path.abspath(current_dir + "/../" + ".secrets")
@@ -26,6 +18,18 @@ if __name__ == '__main__':
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
     secret = open(app_secrets_path)
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read com.test enterprise applications") \
+        .config("spark.mongodb.input.uri", app_conf["mongodb_config"]["uri"]) \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
 
     src_lst = app_conf["source_list"]
     for src in src_lst:
