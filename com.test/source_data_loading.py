a5b3f63536170a3d6067d864ab6e1e255da738ef
faisal <ftokdiya@gmail.com>
2024-03-22 19:56:05 +0530

    initial commit

--- com.test/source_data_loading.py
+++ com.test/source_data_loading.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("Read com.test enterprise applications") \
-        .config("spark.mongodb.input.uri", app_conf["mongodb_config"]["uri"]) \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"]) \
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
